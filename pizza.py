368527f90df1c2a2cc9d742e0ccbd125c731ac71
Ibrahima BAH <ibrahima.bah@startum.academy>
2026-03-30 15:13:35 +0200

    feat: ajout classe Pizza #1

---
+++ pizza.py
@@ -0,0 +1,10 @@
+class Pizza:
+    def __init__(self, name: str, ingredients: list, price: float):
+        self.name = name
+        self.ingredients = ingredients
+        self.price = price
+
+    def __repr__(self):
+        return (f"Pizza(name={self.name}, "
+                f"ingredients={self.ingredients}, "
+                f"price={self.price})")
